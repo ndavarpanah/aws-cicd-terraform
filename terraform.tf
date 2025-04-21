d6fb9b28adae3772efeef977843a86b494ca326e
ndavarpanah <79424197+ndavarpanah@users.noreply.github.com>
2025-04-20 17:39:24 -0700

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
 
   backend "s3" {
     key    = "github-actions-cicd/terraform.tfstate" # the directory/file.tfstate
-    bucket = "ADD_YOUR_BUCKET_NAME_HERE"             # the bucket
-    region = "ADD_YOUR_REGION_NAME_HERE"             # the region
+    bucket = "terraform-state-storage-njvf08832hq8490th134mnqeg"             # the bucket
+    region = "us-west-2"             # the region
   }
 }
